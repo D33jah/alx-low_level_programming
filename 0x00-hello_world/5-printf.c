0cd924323673b556131329702f00b1b296396c0c
D33jah <d33jah29@gmail.com>
2022-06-30 13:47:32 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-/
+#include <string.h>
+
+/**
  * main - Entry point
- * Return: Always 0 (success)
+ * Description: prints "\"with proper grammar, but the outcome is a piece of art,"*
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("\"with proper grammar, but the outcome is a piece of art,");
+
 return (0);
 }
