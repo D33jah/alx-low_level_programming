f50f60f30c19ae7f7f2e5c25b0243d78664af5ef
D33jah <d33jah29@gmail.com>
2022-07-07 10:05:10 -0700

    task 0

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,30 +8,31 @@
  **/
 int main(void)
 {
-	int ch;
-	int n;
-	int m;
+int ch;
+int ch;
+int m;
 
-	for (ch = 48; ch <= 57; ch++)
+for (ch = 48; ch <= 57; ch++)
+{
+	for (n = 49; n <= 57; n++)
 	{
-		for (n = 49; n <= 57; n++)
+		for (m = 50; m <= 57; m++)
 		{
-			for (m = 50; m <= 57; m++)
+			if (n > ch && m > n)
 			{
-				if (n > ch && m > n)
+				putchar(ch);
+				putchar(n);
+				putchar(m);
+				if (ch != 55 || n != 56 || m != 57)
 				{
-					putchar(ch);
-					putchar(n);
-					putchar(m);
-					if (ch != 55 || n != 56 || m != 57)
-					{
-						putchar(44);
-						putchar(32);
-					}
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
-	putchar(10);
-	return (0);
-}	
+}
+putchar(10);
+
+return (0);
+}
